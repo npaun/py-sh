4553989cbebcefdf0df816bcc6638e0788a1a0dd
Nicholas Paun <npaun@cloudflare.com>
2024-08-27 15:15:38 -0700

    Add redirection lol

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 from sh import sh
 
-print(sh.date())
-print(sh.git.ls_files())
+print(repr(sh.date()))
+print(list(sh.git.ls_files()))
 print(sh.git.log(S="import"))
+print(sh.sort() < "/etc/passwd")
+(sh.uptime() >> "uptime.txt").run()
--- sh.py
+++ sh.py
@@ -1,6 +1,78 @@
 import subprocess
 from dataclasses import dataclass, field
+from functools import cached_property
+from typing import Optional
+import os
 
+
+@dataclass
+class ResultAdaptor:
+    argv: list[str]
+    input_file: Optional[str] = None
+    output_file: Optional[str] = None
+    result: Optional[str] = None
+    output_mode: Optional[str] = None
+
+    def invoke(self):
+        if self.result is not None:
+            return
+
+        input_fd = open(self.input_file) if self.input_file else None
+        output_fd = (
+            open(self.output_file, self.output_mode if self.output_mode else "w")
+            if self.output_file
+            else None
+        )
+        capture_output = self.output_file is None
+
+        res = subprocess.run(
+            self.argv, stdin=input_fd, stdout=output_fd, capture_output=capture_output
+        )
+
+        if input_fd:
+            input_fd.close()
+
+        if output_fd:
+            output_fd.flush()
+            output_fd.close()
+
+        res.check_returncode()
+        if res.stdout:
+            self.result = res.stdout.decode()[:-1]
+
+    def run(self):
+        self.invoke()
+        return self.result
+
+    def __str__(self):
+        self.invoke()
+        return self.result
+
+    def __repr__(self):
+        self.invoke()
+        return repr(self.result)
+
+    def __iter__(self):
+        self.invoke()
+        return iter(self.result.splitlines())
+
+    def __lt__(self, input_file):
+        return ResultAdaptor(
+            self.argv, input_file=input_file, output_file=self.output_file
+        )
+
+    def __gt__(self, output_file):
+        return ResultAdaptor(
+            self.argv, input_file=self.input_file, output_file=output_file
+        )
+
+    def __rshift__(self, output_file):
+        return ResultAdaptor(
+            self.argv,
+            input_file=self.input_file,
+            output_file=output_file,
+            output_mode="a",
+        )
 
 
 @dataclass
@@ -11,13 +83,13 @@ class CommandAdaptor:
         argv = [*self.prefix]
         for key, value in kwargs.items():
             if len(key) == 1:
-                option = f'-{key}'
+                option = f"-{key}"
             else:
-                # FIXME: a lot of dodgy tools use a single - for long options too 
+                # FIXME: a lot of dodgy tools use a single - for long options too
                 # FIXME: a lot of dodgy tools use underscores in long option names
-                key_norm = key.replace('_','-')
-                option = f'--{key_norm}'
-            
+                key_norm = key.replace("_", "-")
+                option = f"--{key_norm}"
+
             if value == True:
                 # Acting like a flag
                 argv.extend([option])
@@ -26,13 +98,11 @@ class CommandAdaptor:
                 argv.extend([option, str(value)])
 
         argv.extend(args)
-        res = subprocess.run(argv, capture_output=True)
-        res.check_returncode()
-        return res.stdout.decode()[:-1]
+        return ResultAdaptor(argv)
 
     def __getattr__(self, arg):
         # FIXME might need to run a tool with an underscore in its name
-        return CommandAdaptor([*self.prefix, arg.replace('_','-')])
+        return CommandAdaptor([*self.prefix, arg.replace("_", "-")])
 
 
 sh = CommandAdaptor()
